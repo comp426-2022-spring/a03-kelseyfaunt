396a9c535f357cffa1438ec50ec782770b94356e
kelseyfaunt <59808596+kelseyfaunt@users.noreply.github.com>
2022-04-17 23:14:28 -0400

    testing something

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 const args = require('minimist')(process.argv.slice(2));
 args['port']
-const port = args.port || process.env.PORT || 5000;
+const port = args.port || process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
